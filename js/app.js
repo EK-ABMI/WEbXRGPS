db6409aaf27a06d18a26c75d35cfa448544a5a68
EK-ABMI <84069922+EK-ABMI@users.noreply.github.com>
2021-07-06 16:05:04 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/0.1.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/0.2.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
